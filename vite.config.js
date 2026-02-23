de7ba3ef4bebc43a9b51dfb172b7d94dbd269463
RyoyakS <74259698+RyoyakS@users.noreply.github.com>
2026-02-24 03:42:13 +0800

    add compressed model &  fix vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,8 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/", // 確保資源從根目錄讀取
+  build: {
+    assetsInlineLimit: 0, // 確保大檔案不會被轉成 base64 導致崩潰
+  }
 });
